7dce84216ee282d0cb2ada649d8508942b8b4de2
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-12-02 14:21:14 +0900

    주석 및 오류 수정

--- MyPanel.java
+++ MyPanel.java
@@ -20,35 +20,40 @@ public class MyPanel extends JPanel implements ActionListener
         //setLayout(new FlowLayout());
         JLabel mlName = new JLabel("이름");
         mtfName = new JTextField(18);
-        this.add(mlName); this.add(mtfName);
+        this.add(mlName); 
+        this.add(mtfName);
 
         JLabel mlDept = new JLabel("학과");
         mtfDept = new JTextField(18);
-        mtfDept.setText("글로벌소프트웨어학과");
-        this.add(mlDept); this.add(mtfDept);
+        mtfDept.setText("글로벌소프트웨어학과");//TextField 글로벌소프트웨어학과로 설정
+        this.add(mlDept); 
+        this.add(mtfDept);
 
         JLabel mlAddress = new JLabel("주소");
         mtfAddress = new JTextField(18);
         mtfAddress.setText("서울시...");
-        this.add(mlAddress); this.add(mtfAddress);
+        this.add(mlAddress); 
+        this.add(mtfAddress);
 
         JLabel mlSchoolYear = new JLabel("학년");
-
-        cbSchoolYear = new JComboBox(schoolYear);
-
-        this.add(mlSchoolYear); this.add(cbSchoolYear);
+        cbSchoolYear = new JComboBox(schoolYear); 
+        // Sring List를 파라미터로하는 콤보박스 생성                       
+        this.add(mlSchoolYear); 
+        this.add(cbSchoolYear);
 
         JButton mb = new JButton("추가");
         JButton mbclear = new JButton("clear");
-
-        mb.addActionListener(this); mbclear.addActionListener(this);          
-        
-        this.add(mb); this.add(mbclear); 
+        this.add(mb); 
+        this.add(mbclear); 
+        mb.addActionListener(this); 
+        mbclear.addActionListener(this);          
 
         ta = new JTextArea(10,24);
         this.add(ta);
         this.add(new JScrollPane(ta));
+        //TextArea에 스크롤을 부착
     }
+
     public void actionPerformed(ActionEvent e){
         JButton btn = (JButton)e.getSource();
         if (btn.getText() == "clear"){
@@ -56,6 +61,7 @@ public class MyPanel extends JPanel implements ActionListener
             mtfName.setText("");
             mtfDept.setText("");
             mtfAddress.setText("");
+            //TextArea, TextField 초기화
         }
         else if (btn.getText() == "추가"){
             String name = mtfName.getText();
@@ -63,7 +69,7 @@ public class MyPanel extends JPanel implements ActionListener
             String address = mtfAddress.getText();
             String year = schoolYear[cbSchoolYear.getSelectedIndex()];
             ta.append("이름 : " + name + "\n" + "학과 : "+ dept + "\n" + "학년 : " + year + "\n" + "주소 : "  + address + "\n"
-            + "____________________________" );
+                + "____________________________" );
         }
     }
 }
--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -3,20 +3,18 @@ import java.awt.*;
 /**
  * Write a description of class TextFieldFrame here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315009 황훈태, 2018315039 이윤재,2018315040 김선민)
+ * @version (2019.12.02)
  */
 public class TextFieldFrame extends JFrame
 {
-
+    MyPanel mp = new MyPanel();
     public TextFieldFrame() 
     {
-        setTitle("JAVA_CHAP11");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,150);
-        setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(350,250);
+        this.setVisible(true);
         this.setTitle("실습_4");
-        this.add(new MyPanel());
+        this.add(mp);
     }
-  
 }
